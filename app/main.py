9eda50f59709598c0bda5d6ba2990c8a953c570f
andrii.shchurkov <andrii.shchurkov@irdeto.com>
2018-11-04 22:23:03 +0100

    added static files from the java project

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, send_file
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 3.7 (from the example template)"
+def main():
+    index_path = os.path.join(app.static_folder, 'voting.html')
+    return send_file(index_path)
+
+
+# Everything not declared before (not a Flask route / API endpoint)...
+@app.route('/<path:path>')
+def route_frontend(path):
+    # ...could be a static file needed by the front end that
+    # doesn't use the `static` path (like in `<script src="bundle.js">`)
+    file_path = os.path.join(app.static_folder, path)
+    if os.path.isfile(file_path):
+        return send_file(file_path)
+    # ...or should be handled by the SPA's "router" in front end
+    else:
+        index_path = os.path.join(app.static_folder, 'voting.html')
+        return send_file(index_path)
 
 
 if __name__ == "__main__":
